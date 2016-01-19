b50670e42df3aba2191a2038e06f021f1acbbfd9
Student <student@RU-021.local>
2016-01-19 12:22:35 -0700

    stringReporter

---
+++ stringReporter.js
@@ -0,0 +1,9 @@
+var pet = prompt("What pet do you own or want to own?");
+
+console.log ("You entered " + pet);
+console.log ("There are " + pet.length + " characters in " +pet);
+console.log ("The first letter in " + pet + " is " + pet[0]);
+console.log ("Lowercase: " + pet.toLowerCase());
+console.log ("Uppercase: " + pet.toUpperCase());
+console.log ("I have ALWAYS wanted a " + pet);
+console.log (pet.substring(0,2));
